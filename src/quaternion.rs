d6d35beacf1a13b954f2546b472acf033938c5a2
Maerorr <7hubert7@gmail.com>
2022-11-23 10:31:41 +0100

    feat: word file with tests and last fixes before submission

--- src/quaternion.rs
+++ src/quaternion.rs
@@ -72,8 +72,9 @@ impl Quaternion {
     }
 
     pub fn inverse(&mut self) {
-        let mut quat = Quaternion::new(self.real, self.ivec * -1.0);
-        let divisor = self.real.powi(2) * self.ivec.dot(&self.ivec);
+        let mut quat = self.clone();
+        quat.conjugate();
+        let divisor = self.real * self.real + self.ivec.dot(&self.ivec);
         if approx_eq!(f64, divisor, 0.0, F64Margin { epsilon: f64::EPSILON, ulps: 4 }) {
             print!("Warning: division by zero. Quaternion values were not altered.");
             return;
@@ -87,9 +88,12 @@ impl Quaternion {
     }
 
     pub fn normalize(&mut self) {
-        let inv = 1.0 / self.ivec.length();
-        self.real *= inv;
-        self.ivec *= inv;
+        let inv = 1.0 / self.length();
+        *self *= inv;
+    }
+
+    pub fn length(&self) -> f64 {
+        (self.real * self.real + self.ivec.dot(&self.ivec)).sqrt()
     }
 
     pub fn conjugate(&mut self) {
@@ -101,7 +105,7 @@ impl Quaternion {
     }
 
     pub fn to_string(&self) -> String {
-        let out: String = format!("({:.2}, {:.2}, {:.2}, {:.2})", self.real, self.ivec.x, self.ivec.y, self.ivec.z);
+        let out: String = format!("({:.2}, {:.2}i, {:.2}j, {:.2}k)", self.real, self.ivec.x, self.ivec.y, self.ivec.z);
         out
     }
 }
@@ -244,11 +248,72 @@ impl ops::DivAssign<f64> for Quaternion {
     }
 }
 
+impl PartialEq for Quaternion {
+    fn eq(&self, other: &Quaternion) -> bool {
+        approx_eq!(f64, self.real, other.real, F64Margin { epsilon: f64::EPSILON, ulps: 4 }) &&
+        approx_eq!(f64, self.ivec.x, other.ivec.x, F64Margin { epsilon: f64::EPSILON, ulps: 4 }) &&
+        approx_eq!(f64, self.ivec.y, other.ivec.y, F64Margin { epsilon: f64::EPSILON, ulps: 4 }) &&
+        approx_eq!(f64, self.ivec.z, other.ivec.z, F64Margin { epsilon: f64::EPSILON, ulps: 4 })
+    }
+}
+
 
 #[cfg(test)]
 mod test {
+    use crate::mat4::*;
     use crate::math::as_radians;
+    use crate::vector::*;
     use super::*;
+
+    #[test]
+    fn identity_test() {
+        let quat = Quaternion::identity();
+        println!("\nQUATERNION IDENTITY TEST\n");
+        println!("Calling Quaternion::Identity() returns an identity quaternion: {}", quat.to_string());
+        assert_eq!(quat.real, 1.0);
+        assert_eq!(quat.ivec.x, 0.0);
+        assert_eq!(quat.ivec.y, 0.0);
+        assert_eq!(quat.ivec.z, 0.0);
+    }
+
+    #[test]
+    fn vector_rotation_test() {
+        let mut q = Quaternion::identity();
+        q.rotate(as_radians(90.0), Vector::new(0.0, 0.0, 1.0));
+        let mut mat = Mat4::identity();
+        mat.rotate(as_radians(90.0), Vector::new(0.0, 0.0, 1.0));
+        let mut v1 = Vector::new(1.0, 1.0, 1.0);
+        let mut v2 = Vector::new(1.0, 1.0, 1.0);
+        println!("\nQUATERNION VECTOR ROTATION TEST\n");
+        println!("Let's take a quaternion and a matrix rotated by 90deg on z axis: {},\n{}", q.to_string(), mat.to_string());
+        println!("Create two identical vectors: v1 = {},\nv2 = {}", v1.to_string(), v2.to_string());
+        println!("Rotate one of them with a quaternion and the second with a matrix");
+
+        q.rotate_vec(&mut v1);
+        v2 = v2 * mat;
+
+        println!("v1 = {}, v2 = {}", v1.to_string(), v2.to_string());
+        assert_eq!(v1, v2);
+    }
+
+    #[test]
+    fn hamilton_prod_test() {
+        let mut q1 = Quaternion::new(1.0, Vector::new(2.0, 3.0, 4.0));
+        let mut q2 = Quaternion::new(2.0, Vector::new(3.0, 4.0, 5.0));
+
+        let h = q1.hamilton_product(&q2);
+        let other_h = q2.hamilton_product(&q1);
+
+        println!("\nQUATERNION HAMILTON PRODUCT TEST\n");
+        println!("Let's take two quaternions: q1 = {}, q2 = {}", q1.to_string(), q2.to_string());
+
+        println!("Let's calculate their hamilton product.");
+        println!("the output of this operation should be:\n-36 + 6i + 12j + 12k if it's H(q1, q2)\nor -36 + 8i + 8j + 14k for H(q2, q1)");
+        println!("we revieved: H(q1, q2) = {}, H(q2, q1) = {}", h.to_string(), other_h.to_string());
+
+        assert_eq!(h, Quaternion::new(-36.0, Vector::new(6.0, 12.0, 12.0)));
+        assert_eq!(other_h, Quaternion::new(-36.0, Vector::new(8.0, 8.0, 14.0)));
+    }
 
     #[test]
     // Check if the rotations are correctly applied and then converted to rotation matrix
@@ -256,9 +321,15 @@ mod test {
         let mut quat = Quaternion::identity();
         quat.rotate(as_radians(90.0), Vector::new(1.0, 0.0, 0.0));
         let quat_mat = quat.to_mat4();
+        println!("\nQUATERNION TO MATRIX TEST\n");
+        println!("Let's take a identity quaternion and rotate it by 90deg on x axis: {}", quat.to_string());
+        println!("Then let's transform it into a mat4 frms: {}", quat_mat.to_string());
+
 
         let mut other_mat = Mat4::identity();
         other_mat.rotate(as_radians(90.0), Vector::new(1.0, 0.0, 0.0));
+        println!("now let's take an identity matrix and perform the same operations {}", other_mat.to_string());
+        println!("they are the same, except for floating point precision errors");
 
         let (mut vec1, mut vec2) = (Vector::new(1.0, 1.0, 1.0), Vector::new(1.0, 1.0, 1.0));
         vec1 = vec1 * quat_mat;
@@ -267,12 +338,38 @@ mod test {
     }
 
     #[test]
+    fn normalize_test() {
+        let mut q = Quaternion::new(1.0, Vector::new(2.0, 3.0, 4.0));
+        println!("\nQUATERNION NORMALIZE TEST\n");
+        println!("Let's take a quaternion: {}", q.to_string());
+        println!("Let's normalize it");
+        q.normalize();
+        println!("now it's 'length' should be 1: {:.4}", q.length());
+        assert!(approx_eq!(f64, q.length(), 1.0, F64Margin { epsilon: f64::EPSILON, ulps: 4 }));
+    }
+
+    #[test]
     fn inverse_test() {
-        let mut quat = Quaternion {
-            real: 0.0,
-            ivec: Vector::new(0.0, 0.0, 0.0),
-        };
-        quat.inverse();
-        // will print an error and not change values
+        let mut quat = Quaternion::new(1.0, Vector::new(2.0, 3.0, 4.0));
+        let mut inverse = quat.clone();
+        inverse.inverse();
+        println!("\nQUATERNION INVERSE TEST\n");
+        println!("Let's take a quaternion: {}", quat.to_string());
+        println!("Let's calculate its inverse: {}", inverse.to_string());
+        println!("Let's multiply the quaternion by its inverse");
+        quat *= inverse;
+        println!("The result should be an identity quaternion: {}", quat.to_string());
+        assert_eq!(quat, Quaternion::new(1.0, Vector::new(0.0, 0.0, 0.0)));
+    }
+
+    #[test]
+    fn conjugate_test() {
+        let mut quat = Quaternion::new(1.0, Vector::new(2.0, 3.0, 4.0));
+        let mut conjugate = quat.clone();
+        conjugate.conjugate();
+        println!("\nQUATERNION CONJUGATE TEST\n");
+        println!("Let's take a quaternion: {}", quat.to_string());
+        println!("Let's calculate its conjugate: {}", conjugate.to_string());
+        assert_eq!(conjugate, Quaternion::new(1.0, Vector::new(-2.0, -3.0, -4.0)));
     }
 }
